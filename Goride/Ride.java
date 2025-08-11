5b875f15a84f31b74024e55c7c9934a19b11f029
NibinBijuCheriyan <nibin00077@gmail.com>
2025-08-11 15:46:26 +0530

    Delete Goride directory

--- Goride/Ride.java
+++
@@ -1,9 +0,0 @@
-// Abstract superclass
-public abstract class Ride {
-    protected double distance; 
-
-    public Ride(double distance) {
-        this.distance = distance;
-    }
-    public abstract double calculateFare();
-}
